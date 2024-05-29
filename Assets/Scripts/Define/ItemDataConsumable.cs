1dd2f16dd3c884527b3a2fce27e17e2433732305
Devwons93 <jion933@gmail.com>
2024-05-29 22:08:50 +0900

    feat : Item 프리펩 생성

---
+++ Assets/Scripts/Define/ItemDataConsumable.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class ItemDataConsumable
+{
+    public EConsumableType type;
+    public float value;
+}
---
+++ Assets/Scripts/Enums/Enums.cs
@@ -0,0 +1,14 @@
+using System;
+
+public enum EItemType
+{
+    EQUIPABLE,
+    CONSUMABLE,
+    RESOURCE
+}
+
+public enum EConsumableType
+{
+    HEALTH,
+    HUNGER
+}
